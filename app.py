d3e145f46050a0a7e16051d41901910382d1985d
patchgi <pacchigi0910@gmail.com>
2016-09-21 22:05:40 +0900

    follow pep8

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#coding utf-8
+# coding:utf-8
 import markdown
 import sys
 import os
@@ -10,6 +10,7 @@ app = QApplication(sys.argv)
 web = QWebView()
 printer = QPrinter()
 
+
 def load_finished():
     global web
     global printer
@@ -17,39 +18,39 @@ def load_finished():
     web.print_(printer)
     QApplication.exit()
 
+
 def md2html(_fileName):
-	md=markdown.Markdown()
-	tmpText = u'<html><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8"></head><body>' 
-	text=""
-	for i in open(_fileName,"r"):
-		 text+=unicode(i,'utf-8')
-	
-	text=md.convert(text)
-	tmpText+=text+u"</body></html>"
-	fn=open("test.html","w")
-	fn.write(tmpText.encode("utf-8"))
-	fn.close()
-	
+    md = markdown.Markdown()
+    tmpText =u'<html><head><meta http-equiv="Content-Type"content="text/html;harset=UTF-8"></head><body>'
+    text = ""
+    for i in open(_fileName, "r"):
+        text += unicode(i, 'utf-8')
+        text = md.convert(text)
+        tmpText += text+u"</body></html>"
+        fn = open("test.html", "w")
+        fn.write(tmpText.encode("utf-8"))
+        fn.close()
+
+
 def html2pdf():
-	global app,param
-	outputFile=param[1].split(".")[0]+".pdf"
-	printer.setOutputFormat(QPrinter.PdfFormat)
-	printer.setOrientation(QPrinter.Landscape)
-	printer.setPageSize(QPrinter.A4)
-	printer.setResolution(QPrinter.HighResolution)
-	printer.setOutputFileName(outputFile)
-	
-	web.loadFinished.connect(load_finished)
-	web.load(QUrl.fromLocalFile(os.path.abspath('test.html')))
-	
-	sys.exit(app.exec_())
+    global app, param
+    outputFile = param[1].split(".")[0]+".pdf"
+    printer.setOutputFormat(QPrinter.PdfFormat)
+    printer.setOrientation(QPrinter.Landscape)
+    printer.setPageSize(QPrinter.A4)
+    printer.setResolution(QPrinter.HighResolution)
+    printer.setOutputFileName(outputFile)
+    web.loadFinished.connect(load_finished)
+    web.load(QUrl.fromLocalFile(os.path.abspath('test.html')))
+    sys.exit(app.exec_())
+
 
 def main():
-	global param
-	md2html(param[1])
-	html2pdf()
-	
-if __name__=="__main__":
-	param=sys.argv
-	main()
-	
+    global param
+    md2html(param[1])
+    html2pdf()
+
+
+if __name__ == "__main__":
+    param = sys.argv
+    main()
